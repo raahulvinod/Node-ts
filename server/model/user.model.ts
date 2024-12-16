2dfd94c7c8ab8ab1d111a05e7788b13ffe5a3a3c
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-12-16 15:43:45 +0530

    feat(model): add user model and schema

---
+++ server/model/user.model.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
+
+export const getUsers = () => User.find();
+export const getUsersByEmail = (email: String) => User.findOne({ email });
+export const getUserBySessionToken = (sessionToken: String) =>
+  User.findOne({
+    'authentication.sessionToken': sessionToken,
+  });
+export const getUserById = (id: string) => User.findById(id);
+export const createUser = (values: Record<string, any>) =>
+  new User(values).save().then((user) => user.toObject());
+export const deleteUsersById = (id: string) =>
+  User.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) =>
+  User.findByIdAndUpdate({ id, values });
